0fa5dfc31bb93efa302ecaf60edd18e80d5b87f0
Beah <beah.dev@gmail.com>
2021-01-29 18:01:55 -0400

    Lint, rotas e state

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
-import { delBasePath } from 'next/dist/next-server/lib/router/router'
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-import QuizBackground from '../src/components/QuizBackground'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -25,16 +29,49 @@ export const QuizContainer = styled.div`
 // background-position: center;
 // `;
 
-
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>
+          {db.title}
+        </title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
-                <h1>O quanto você conhece sobre o Universo</h1>
+            <h1>O quanto você conhece sobre o Universo</h1>
           </Widget.Header>
-          
+
+          <Widget.Content>
+            <form onSubmit={function (event) {
+              event.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <input
+                onChange={function (event) {
+                  setName(event.target.value);
+                }}
+                placeholder="Seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar como
+                {' '}
+                {name}
+              </button>
+            </form>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Header>
+            <h1>O quanto você conhece sobre o Universo</h1>
+          </Widget.Header>
+
           <Widget.Content>
             <p>Bora começar!</p>
           </Widget.Content>
@@ -42,8 +79,9 @@ export default function Home() {
 
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/Beatriz-Paes"/>
+
+      <GitHubCorner projectUrl="https://github.com/Beatriz-Paes" />
     </QuizBackground>
-  
-    )
+
+  );
 }
---
+++ pages/quiz.js
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import db from '../db.json';
+
+export default function QuizDaGaleraPage() {
+  return (
+    <div>hello</div>
+  );
+}
